aa5b33a25440fcd214e47fdf6ccd2db3a96179f5
Hapro Bishop <acfowo45@gmail.com>
2022-03-15 19:12:04 +0300

    Добавьте файлы проекта.

---
+++ AllYFinder/YFinder2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AllYFinder
+{/// <summary>
+ /// Класс для метода поиска y =3*tg (x)
+ /// </summary>
+    public static class YFinder2
+    {
+        public static double[] YFind(int[] xMas)
+        {
+            double[] yMas = new double[xMas.Length];
+            for (int i = 0; i < xMas.Length; i++)
+            {
+                yMas[i] = 3 * Math.Tan(xMas[i]);
+            }
+            return yMas;
+        }
+    }
+}
---
+++ DataGridFiller/VisualArray.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+
+namespace DataGridFiller
+{
+    /// <summary>
+    /// Класс для связывания DataGrid с массивом данных
+    /// </summary>
+    public static class VisualArray
+    {
+        /// <summary>
+        /// Заполнение для одномерного массива
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static DataTable ToDataTable<T>(this T[] arr)
+        {
+            var res = new DataTable();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                res.Columns.Add("col" + (i + 1), typeof(T));
+            }
+            var row = res.NewRow();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                row[i] = arr[i];
+            }
+            res.Rows.Add(row);
+            return res;
+        }
+    }
+}
